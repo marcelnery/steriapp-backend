7e520c75f6d4061898f5eee1e949353523096156
Marcel Scalco Nery <marcelodontotec@gmail.com>
2026-03-09 19:51:37 -0300

    fix public path vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,13 +52,13 @@ const __dirname = path.dirname(__filename);
 // ===============================
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
-
+const publicPath = path.join(process.cwd(), 'src/public');
+app.use(express.static(publicPath));
 // ===============================
 // ROTA RAIZ
 // ===============================
 app.get('/', (req, res) => {
-  res.send('🚀 Backend SteriLink rodando com sucesso');
+  res.send('🚀 Backend SteriLink rodando com sucesso AGORA');
 });
 
 // ===============================
